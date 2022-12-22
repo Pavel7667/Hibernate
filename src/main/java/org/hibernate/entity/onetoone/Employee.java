db9535d6482b4e4c1479d659d7e71aaad0b1ec85
pavel.hnelytsia <pavelgnelicya@gmail.com>
2022-12-22 14:09:49 +0200

    One to One DB (Uni-Direction)
    -@OneToOne (cascade = doAll or spec Operation)
    -@JoinColumn(name = "idFromAnotherTable")

---
+++ src/main/java/org/hibernate/entity/onetoone/Employee.java
@@ -0,0 +1,102 @@
+package org.hibernate.entity.onetoone;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+/**
+ * Class Employee Default POJO clas for ORM work
+ *
+ * @Entity for connection with DB
+ * @Table to connection with Table with same name ="asThis"
+ */
+@Entity
+@Table(name = "employees")
+public class Employee {
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "details_id")
+    private Detail detail;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column
+    private int id;
+    @Column
+    private String name;
+    @Column
+    private String surname;
+    @Column
+    private String department;
+    @Column
+    private int salary;
+
+    // POJO //
+
+    public Employee() {
+    }
+
+    public Employee(String name, String surname, String department, int salary) {
+        this.name = name;
+        this.surname = surname;
+        this.department = department;
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return "EntityStaff{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", department='" + department + '\'' +
+                ", salary=" + salary +
+                '}';
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+}
