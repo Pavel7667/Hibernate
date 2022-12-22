5ddacd4dd8972fc5dfbe3602b5cc143020e23857
pavel.hnelytsia <pavelgnelicya@gmail.com>
2022-12-22 11:06:53 +0200

    - add hibernate files - add ConnectorJ - create POJO class

---
+++ src/main/java/org/hibernate/EntityStaff.java
@@ -0,0 +1,92 @@
+package org.hibernate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ * Class EntityStaff Default POJO clas for ORM work
+ *
+ * @Entity for connection with DB
+ * @Table to connection with Table with same name ="asThis"
+ */
+@Entity
+@Table(name = "staff")
+public class EntityStaff {
+
+    @Id
+    @Column
+    private int id;
+    @Column
+    private String name;
+    @Column
+    private String surname;
+    @Column
+    private String department;
+    @Column
+    private int salary;
+
+    // POJO //
+
+    public EntityStaff() {
+    }
+
+    public EntityStaff(String name, String surname, String department, int salary) {
+        this.name = name;
+        this.surname = surname;
+        this.department = department;
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return "EntityStaff{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", department='" + department + '\'' +
+                ", salary=" + salary +
+                '}';
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+}
